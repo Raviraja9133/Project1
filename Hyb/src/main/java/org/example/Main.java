2113e218f3f4f9c45b5e818afd1b98d209cc51a3
Raviraja9133 <sanaraviraja@gmail.com>
2025-05-24 19:29:50 +0530

    new Line Added

---
+++ Hyb/src/main/java/org/example/Main.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+import java.lang.module.Configuration;
+
+public class Main {
+    public static void main(String[] args) {
+        Students stu1 = new Students();
+        stu1.setId(101);
+        stu1.setName("SANA");
+        stu1.setTech("JAVA");
+
+
+
+
+
+
+    }
+}
---
+++ Hyb/src/main/java/org/example/Students.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import lombok.Data;
+import lombok.ToString;
+
+@Data
+@ToString
+
+public class Students {
+
+    private int id;
+    private String name;
+    private String tech;
+
+}
